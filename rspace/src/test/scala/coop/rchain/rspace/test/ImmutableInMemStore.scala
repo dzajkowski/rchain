cd8bfd94ad715d6e442f9b29b9445bb9687ee3d4
Dominik Zajkowski <dominik@pyrofex.net>
2018-05-29 13:44:00 +0200

    WIP txn

--- rspace/src/test/scala/coop/rchain/rspace/test/ImmutableInMemStore.scala
+++ rspace/src/test/scala/coop/rchain/rspace/test/ImmutableInMemStore.scala
@@ -19,85 +19,128 @@ class ImmutableInMemStore[C, P, A, K <: Serializable] private (
     extends IStore[C, P, A, K]
     with ITestableStore[C, P] {
 
-  private[this] val keysRef = new SyncVar[Map[String, Seq[C]]].init(_keys)
-  private[this] val waitingContinuationsRef =
-    new SyncVar[Map[String, Seq[WaitingContinuation[P, K]]]].init(_waitingContinuations)
-  private[this] val dataRef = new SyncVar[Map[String, Seq[Datum[A]]]].init(_data)
-  private[this] val joinRef = new SyncVar[Map[C, Set[String]]].init(_joinMap)
+  case class State(
+      keys: Map[String, Seq[C]],
+      continuations: Map[String, Seq[WaitingContinuation[P, K]]],
+      data: Map[String, Seq[Datum[A]]],
+      joins: Map[C, Set[String]]
+  ) {}
+
+  object State {
+    def apply(
+        keys: Map[String, Seq[C]],
+        continuations: Map[String, Seq[WaitingContinuation[P, K]]],
+        data: Map[String, Seq[Datum[A]]],
+        joins: Map[C, Set[String]]
+    ): State = {
+      // defensive copy of potentially mutable data
+      val copy = continuations.map {
+        case (k, cs) =>
+          k -> cs.map(wk => wk.copy(continuation = ImmutableInMemStore.roundTrip(wk.continuation)))
+      }
+      new State(keys, copy, data, joins)
+    }
+  }
+
+  val state: Transaction[State] =
+    new LockingTransaction[State](State(_keys, _waitingContinuations, _data, _joinMap))
 
   private[rspace] type H = String
 
-  private[rspace] type T = Unit
-
-  private[rspace] def hashChannels(cs: Seq[C])(implicit sc: Serialize[C]): H =
-    printHexBinary(InMemoryStore.hashBytes(cs.flatMap(sc.encode).toArray))
-
-  private[this] def putCs(txn: T, channels: Seq[C]): Unit =
-    keysRef.update(_ + (hashChannels(channels) -> channels))
-
-  private[rspace] def getChannels(txn: T, s: H): Seq[C] =
-    keysRef.get.getOrElse(s, Seq.empty[C])
-
-  private[rspace] def createTxnRead(): Unit = ()
-
-  private[rspace] def createTxnWrite(): Unit = ()
+  private[rspace] type T = Transaction[State]
 
   private[rspace] def withTxn[R](txn: T)(f: T => R): R = f(txn)
 
-  private[rspace] def collectGarbage(txn: T,
-                                     channelsHash: H,
-                                     dataCollected: Boolean = false,
-                                     waitingContinuationsCollected: Boolean = false,
-                                     joinsCollected: Boolean = false): Unit =
-    collectGarbage(channelsHash)
+  trait Transaction[DATA] {
+    def read: DATA
+    def write(f: DATA => DATA): Unit
+  }
 
-  private[this] def collectGarbage(key: H): Unit =
-    RichSyncVar.update3(waitingContinuationsRef, keysRef, dataRef) {
-      (waitingContinuations, keys, data) =>
-        val as    = data.get(key).exists(_.nonEmpty)
-        val psks  = waitingContinuations.get(key).exists(_.nonEmpty)
-        val cs    = keys.getOrElse(key, Seq.empty[C])
-        val joins = cs.size == 1 && joinRef.get.contains(cs.head)
-        (
-          Some(waitingContinuations)
-            .filter(_ => psks)
-            .getOrElse(waitingContinuations - key),
-          Some(keys)
-            .filter(_ => as || psks || joins)
-            .getOrElse(keys - key),
-          Some(data)
-            .filter(_ => as)
-            .getOrElse(data - key)
-        )
+  object LockingTransaction {
+    val DEFAULT_TIMEOUT_MS: Long = 100
+  }
+
+  class LockingTransaction[DATA](state: DATA) extends Transaction[DATA] {
+    val stateRef: SyncVar[DATA] = new SyncVar[DATA]().init(state)
+
+    def read: DATA = stateRef.get(LockingTransaction.DEFAULT_TIMEOUT_MS).get
+
+    def write(f: DATA => DATA): Unit = {
+      val prev = stateRef.take(LockingTransaction.DEFAULT_TIMEOUT_MS)
+      try {
+        val next = f(prev)
+        stateRef.put(next)
+      } catch {
+        case ex: Throwable =>
+          stateRef.put(prev)
+          throw ex
+      }
     }
+  }
+
+  private[this] class ReentrantTransaction[DATA](state: DATA) extends Transaction[DATA] {
+    var _state: DATA = state
+
+    def read: DATA = state
+
+    def write(f: DATA => DATA): Unit = {
+      val prev = _state
+      try {
+        _state = f(prev)
+      } catch {
+        case ex: Throwable =>
+          _state = prev
+          throw ex
+      }
+    }
+  }
+
+  private[this] def putCs(txn: T, channels: Seq[C]): Unit =
+    txn.write(
+      state => state.copy(keys = state.keys + (hashChannels(channels) -> channels))
+    )
+
+  private[rspace] def getChannels(txn: T, s: H): Seq[C] =
+    txn.read.keys.getOrElse(s, Seq.empty[C])
 
   private[rspace] def putDatum(txn: T, channels: Seq[C], datum: Datum[A]): Unit =
-    dataRef.update { data =>
-      val key = hashChannels(channels)
-      putCs(txn, channels)
-      val datums = data.getOrElse(key, Seq.empty[Datum[A]])
-      data + (key -> (datum +: datums))
+    txn.write {
+      case state @ State(keys, waitingContinuations, data, joins) =>
+        val key          = hashChannels(channels)
+        val reentrantTxn = new ReentrantTransaction(state)
+        putCs(reentrantTxn, channels)
+        val datums = data.getOrElse(key, Seq.empty[Datum[A]])
+        State(reentrantTxn._state.keys,
+              waitingContinuations,
+              data + (key -> (datum +: datums)),
+              joins)
     }
 
   private[rspace] def putWaitingContinuation(txn: T,
                                              channels: Seq[C],
                                              continuation: WaitingContinuation[P, K]): Unit =
-    waitingContinuationsRef.update { waitingContinuations =>
-      val key = hashChannels(channels)
-      putCs(txn, channels)
-      val forKey: Seq[WaitingContinuation[P, K]] =
-        waitingContinuations.getOrElse(key, Seq.empty[WaitingContinuation[P, K]])
-      waitingContinuations + (key -> (continuation +: forKey))
+    txn.write {
+      case state @ State(keys, waitingContinuations, data, joins) =>
+        val key          = hashChannels(channels)
+        val reentrantTxn = new ReentrantTransaction(state)
+        putCs(reentrantTxn, channels)
+        val forKey: Seq[WaitingContinuation[P, K]] =
+          waitingContinuations.getOrElse(key, Seq.empty[WaitingContinuation[P, K]])
+
+        State(reentrantTxn._state.keys,
+              waitingContinuations + (key -> (continuation +: forKey)),
+              data,
+              joins)
     }
 
   private[rspace] def getData(txn: T, channels: Seq[C]): Seq[Datum[A]] =
-    dataRef.get.getOrElse(hashChannels(channels), Seq.empty[Datum[A]])
+    txn.read.data.getOrElse(hashChannels(channels), Seq.empty[Datum[A]])
 
   private[rspace] def getWaitingContinuation(txn: T, curr: Seq[C]): Seq[WaitingContinuation[P, K]] =
-    waitingContinuationsRef.get
+    txn.read.continuations
       .getOrElse(hashChannels(curr), Seq.empty[WaitingContinuation[P, K]])
       .map { (wk: WaitingContinuation[P, K]) =>
-        wk.copy(continuation = ImmutableInMemStore.roundTrip(wk.continuation))
+        wk.copy(continuation = ImmutableInMemStore.roundTrip(wk.continuation)) // bake this into State?
       }
 
   private[rspace] def removeDatum(txn: T, channel: C, index: Int): Unit =
@@ -105,92 +148,153 @@ class ImmutableInMemStore[C, P, A, K <: Serializable] private (
 
   private[rspace] def removeDatum(txn: T, channels: Seq[C], index: Int): Unit = {
     val key = hashChannels(channels)
-    dataRef.update(data =>
-      data.get(key).map(as => data + (key -> dropIndex(as, index))).getOrElse(data))
-    collectGarbage(key)
+    txn.write(
+      state =>
+        state.copy(
+          data = state.data
+            .get(key)
+            .map(as => state.data + (key -> dropIndex(as, index)))
+            .getOrElse(state.data)))
+    collectGarbage(txn, key)
   }
 
   private[rspace] def removeWaitingContinuation(txn: T, channels: Seq[C], index: Int): Unit = {
     val key = hashChannels(channels)
-    waitingContinuationsRef.update(wc =>
-      wc.get(key).map(x => wc + (key -> dropIndex(x, index))).getOrElse(wc))
-    collectGarbage(key)
+    txn.write(
+      state =>
+        state.copy(
+          continuations = state.continuations
+            .get(key)
+            .map(x => state.continuations + (key -> dropIndex(x, index)))
+            .getOrElse(state.continuations)))
+    collectGarbage(txn, key)
   }
 
-  //how volatile is this?
-  private[rspace] def removeAll(txn: Unit, channels: Seq[C]): Unit = {
+  private[rspace] def removeAll(txn: T, channels: Seq[C]): Unit = {
     val key = hashChannels(channels)
-    RichSyncVar.update2(waitingContinuationsRef, dataRef) { (waitingContinuations, data) =>
-      (waitingContinuations + (key -> Seq.empty), data + (key -> Seq.empty))
-    }
-    for (c <- channels) removeJoin(txn, c, channels)
+    txn.write(
+      state =>
+        state.copy(continuations = state.continuations + (key -> Seq.empty),
+                   data = state.data + (key                   -> Seq.empty)))
+    for (c <- channels) removeJoin(txn, c, channels) // needs to be in the same txn
   }
 
   private[rspace] def addJoin(txn: T, c: C, cs: Seq[C]): Unit =
-    joinRef.update(joins => joins + (c -> (joins.getOrElse(c, Set.empty) + hashChannels(cs))))
+    txn.write(
+      state =>
+        state.copy(
+          joins = state.joins + (c -> (state.joins.getOrElse(c, Set.empty) + hashChannels(cs)))))
 
   private[rspace] def getJoin(txn: T, c: C): Seq[Seq[C]] =
-    joinRef.get.getOrElse(c, Set.empty[String]).toList.map(getChannels(txn, _))
+    txn.read.joins.getOrElse(c, Set.empty[String]).toList.map(getChannels(txn, _))
 
   private[rspace] def removeJoin(txn: T, c: C, cs: Seq[C]): Unit = {
     val joinKey = hashChannels(Seq(c))
-    val csKey   = hashChannels(cs)
-    RichSyncVar.update2(waitingContinuationsRef, joinRef) { (waitingContinuations, joins) =>
-      val hasContinuationValues = waitingContinuations.get(csKey).forall(_.isEmpty)
-      def dropKey =
-        (value: Set[String]) =>
-          value - csKey match {
-            case r if r.isEmpty => joins - c
-            case removed        => joins + (c -> removed)
-        }
-      val result =
-        Option(joins).filter(_ => !hasContinuationValues).getOrElse {
-          joins
-            .get(c)
-            .map(dropKey)
-            .getOrElse(joins)
-        }
-      (waitingContinuations, result)
+    txn.write {
+      case State(keys, waitingContinuations, data, joins) =>
+        val csKey                 = hashChannels(cs)
+        val hasContinuationValues = waitingContinuations.get(csKey).forall(_.isEmpty)
+
+        def dropKey =
+          (value: Set[String]) =>
+            value - csKey match {
+              case r if r.isEmpty => joins - c
+              case removed        => joins + (c -> removed)
+          }
+
+        val result =
+          Option(joins).filter(_ => !hasContinuationValues).getOrElse {
+            joins
+              .get(c)
+              .map(dropKey)
+              .getOrElse(joins)
+          }
+        State(keys, waitingContinuations, data, result)
     }
-    collectGarbage(joinKey)
+    collectGarbage(txn, joinKey)
   }
 
   private[rspace] def removeAllJoins(txn: T, c: C): Unit = {
-    joinRef.update(_ - c)
-    collectGarbage(hashChannels(Seq(c)))
+    txn.write(state => state.copy(joins = state.joins - c))
+    collectGarbage(txn, hashChannels(Seq(c)))
   }
 
   def close(): Unit = ()
 
   def getPatterns(txn: T, channels: Seq[C]): Seq[Seq[P]] =
-    waitingContinuationsRef.get.getOrElse(hashChannels(channels), Nil).map(_.patterns)
+    txn.read.continuations.getOrElse(hashChannels(channels), Nil).map(_.patterns)
 
-  def clear(): Unit = {
-    keysRef.replace(Map.empty[String, Seq[C]])
-    waitingContinuationsRef.replace(Map.empty[String, Seq[WaitingContinuation[P, K]]])
-    dataRef.replace(Map.empty[String, Seq[Datum[A]]])
-    joinRef.replace(Map.empty[C, Set[String]])
-  }
+  def clear(): Unit =
+    withTxn(createTxnWrite()) { txn =>
+      txn.write(
+        _ =>
+          State(Map.empty[String, Seq[C]],
+                Map.empty[String, Seq[WaitingContinuation[P, K]]],
+                Map.empty[String, Seq[Datum[A]]],
+                Map.empty[C, Set[String]]))
+    }
 
   def getStoreSize: StoreSize =
-    StoreSize(0,
-              (keysRef.get.size +
-                waitingContinuationsRef.get.size +
-                dataRef.get.size +
-                joinRef.get.size).toLong)
+    withTxn(createTxnRead()) { txn =>
+      val state = txn.read
+      StoreSize(0,
+                (state.keys.size +
+                  state.continuations.size +
+                  state.data.size +
+                  state.joins.size).toLong)
+    }
 
   def isEmpty: Boolean =
-    waitingContinuationsRef.get.isEmpty && dataRef.get.isEmpty && keysRef.get.isEmpty && joinRef.get.isEmpty
+    withTxn(createTxnRead()) { txn =>
+      val state = txn.read
+      state.continuations.isEmpty && state.data.isEmpty && state.keys.isEmpty && state.joins.isEmpty
+    }
 
   def toMap: Map[Seq[C], Row[P, A, K]] =
-    keysRef.get
-      .map {
-        case (hash, cs) =>
-          val data = dataRef.get.getOrElse(hash, Seq.empty[Datum[A]])
-          val wks =
-            waitingContinuationsRef.get.getOrElse(hash, Seq.empty[WaitingContinuation[P, K]])
-          (cs, Row(data, wks))
-      }
+    withTxn(createTxnRead()) { txn =>
+      val state = txn.read
+      state.keys
+        .map {
+          case (hash, cs) =>
+            val data = state.data.getOrElse(hash, Seq.empty[Datum[A]])
+            val wks =
+              state.continuations.getOrElse(hash, Seq.empty[WaitingContinuation[P, K]])
+            (cs, Row(data, wks))
+        }
+    }
+
+  private[rspace] def hashChannels(cs: Seq[C])(implicit sc: Serialize[C]): H =
+    printHexBinary(InMemoryStore.hashBytes(cs.flatMap(sc.encode).toArray))
+
+  private[rspace] def createTxnRead(): T = state
+
+  private[rspace] def createTxnWrite(): T = state
+
+  private[rspace] def collectGarbage(txn: T,
+                                     channelsHash: H,
+                                     dataCollected: Boolean = false,
+                                     waitingContinuationsCollected: Boolean = false,
+                                     joinsCollected: Boolean = false): Unit =
+    collectGarbage(txn, channelsHash)
+
+  private[this] def collectGarbage(txn: T, key: H): Unit = txn.write {
+    case State(keys, waitingContinuations, data, joins) =>
+      val as         = data.get(key).exists(_.nonEmpty)
+      val psks       = waitingContinuations.get(key).exists(_.nonEmpty)
+      val cs         = keys.getOrElse(key, Seq.empty[C])
+      val joinExists = cs.size == 1 && joins.contains(cs.head)
+
+      val nwc = Some(waitingContinuations)
+        .filter(_ => psks)
+        .getOrElse(waitingContinuations - key)
+      val nk = Some(keys)
+        .filter(_ => as || psks || joinExists)
+        .getOrElse(keys - key)
+      val nd = Some(data)
+        .filter(_ => as)
+        .getOrElse(data - key)
+      State(nk, nwc, nd, joins)
+  }
 }
 
 object ImmutableInMemStore {
@@ -210,57 +314,6 @@ object ImmutableInMemStore {
       _data = Map.empty[String, Seq[Datum[A]]],
       _joinMap = Map.empty[C, Set[String]]
     )
-
-  object RichSyncVar {
-    def update3[R1, R2, R3](ref1: SyncVar[R1], ref2: SyncVar[R2], ref3: SyncVar[R3])(
-        f: (R1, R2, R3) => (R1, R2, R3)): Unit = {
-      val prev1 = ref1.take(DEFAULT_TIMEOUT_MS)
-      try {
-        val prev2 = ref2.take(DEFAULT_TIMEOUT_MS)
-        try {
-          val prev3 = ref3.take(DEFAULT_TIMEOUT_MS)
-          try {
-            val (next1, next2, next3) = f(prev1, prev2, prev3)
-            ref1.put(next1)
-            ref2.put(next2)
-            ref3.put(next3)
-          } catch {
-            case ex: Throwable =>
-              ref3.put(prev3)
-              throw ex
-          }
-        } catch {
-          case ex: Throwable =>
-            ref2.put(prev2)
-            throw ex
-        }
-      } catch {
-        case ex: Throwable =>
-          ref1.put(prev1)
-          throw ex
-      }
-    }
-
-    def update2[R1, R2](ref1: SyncVar[R1], ref2: SyncVar[R2])(f: (R1, R2) => (R1, R2)): Unit = {
-      val prev1 = ref1.take(DEFAULT_TIMEOUT_MS)
-      try {
-        val prev2 = ref2.take(DEFAULT_TIMEOUT_MS)
-        try {
-          val (next1, next2) = f(prev1, prev2)
-          ref1.put(next1)
-          ref2.put(next2)
-        } catch {
-          case ex: Throwable =>
-            ref2.put(prev2)
-            throw ex
-        }
-      } catch {
-        case ex: Throwable =>
-          ref1.put(prev1)
-          throw ex
-      }
-    }
-  }
 
   implicit class RichSyncVar[R](ref: SyncVar[R]) {
 
